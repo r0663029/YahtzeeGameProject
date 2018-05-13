646b94d5b2dcac82bd3e8f108a272b1c7ef63cbd
KristofVandenbroucke <Kristof.vandenbrouck@gmail.com>
2018-05-13 19:39:57 +0200

    Add extra methods to facade class
    Verschillende methodes toegevoegd aan de facade klass:
    	-Een switchPlayerMethod om van speler te veranderen
    	-verschillende methodes voor het berekenen/opvragen van de scores obv een categorie
    	-Overzicht vragen van al de mogelijke categorieen of een specifieke categorie

--- OOO_Part2/yahtzee/src/yahtzee/domain/YahtzeeFacade.java
+++ OOO_Part2/yahtzee/src/yahtzee/domain/YahtzeeFacade.java
@@ -1,5 +1,7 @@
 package yahtzee.domain;
 
+import yahtzee.domain.score.ScoreCategory;
+
 import java.util.List;
 import java.util.ArrayList;
 
@@ -9,7 +11,6 @@ import java.util.ArrayList;
 public class YahtzeeFacade {
 
     private final PlayerGroup playerGroup;
-    private Turn turn;
 
     public YahtzeeFacade() {
 	playerGroup = new PlayerGroup();
@@ -60,51 +61,49 @@ public class YahtzeeFacade {
 	return true;
     }
     /**
-    generates random integers based on how many spots there are still available.
+    @return List of random integers based on how many spots there are still available.
     This is calculated by subtracting 5 minus the dices places aside.
     */
     public List<Integer> roll () {
         return playerGroup.getCurrentPlayer().getTurn().roll();
     }
 
+	/**
+	 @return Turn object for the current active player
+	 */
+    public Turn getTurnOfCurrentPlayer() {return getCurrentPlayer().getTurn();}
+
     /**
-     Set a list of dices aside chosen by the player
+	 @param dieIndex representing the index in de list of thrown dice.
      */
 
-    public void setAside(List<Integer> diceIndices) {
-        turn.setAside(diceIndices);
+    public void setAside(int dieIndex) {
+        getTurnOfCurrentPlayer().setAside(dieIndex);
     }
 
     /**
-     Method to get a list of all the dices which have been placed aside
+	 @return List of integers representing all the dices placed aside
      */
 
     public List<Integer> getDiceAside() {
-        return turn.getDicesAside();
+        return getTurnOfCurrentPlayer().getDicesAside();
     }
 
     /**
-     Method to get a list of all the dices thrown
+	 @return List of integers representing all the dices thrown
      */
 
     public List<Integer> getDiceThrown() {
-        return turn.getDicesThrown();
+        return getTurnOfCurrentPlayer().getDicesThrown();
     }
 
     /**
      Method to check if a person is allowed to roll the dice
+	 * @return true if a player may roll. False if not.
      */
 
     public boolean mayRoll() {
         return playerGroup.getCurrentPlayer().getTurn().mayRoll();
-    }
-
-    /**
-     Method used to transfer a chosen dice from the thrown dice list to the aside dice list
-     */
-
-    public void chooseDice(int diceId) {
-        this.getCurrentPlayer().getTurn().chooseDie(diceId);
     }
 
     /**
@@ -116,11 +115,91 @@ public class YahtzeeFacade {
     }
 
     /**
-     * Get the current playergroup
+     * Get the current playerGroup
+	 * @return PlayerGroup consisting of all the players
      */
 
     public PlayerGroup getPlayerGroup() {
         return playerGroup;
     }
 
+	/**
+	 *
+	 * @return List of all the registered players in the playerGroup
+	 */
+
+	public List<Player> getRegisteredPlayers() { return getPlayerGroup().getPlayers();}
+
+	/**
+	 * Get the Scoreboard for the current player.
+	 * @return PlayerGroup consisting of all the players
+	 */
+
+	public Scoreboard getScoreboard () {
+		return getCurrentPlayer().getScoreboard();
+	}
+
+	/**
+	 * Get the CategoryList for the current player.
+	 * @return List<ScoreCategory> for all the categories for the current player.
+	 */
+
+	public List<ScoreCategory> getCategoryList() {
+		return getCurrentPlayer().getScoreboard().getCategoryList();
+	}
+
+	/**
+	 * Get the CategoryList for the current player .
+	 * @return PlayerGroup consisting of all the categories as Strings
+	 */
+
+	public List<String> getCategoryListStrings() {
+		return getCurrentPlayer().getScoreboard().getCategoryListAsString();
+	}
+
+	/**
+	 * Get the Scoreboard for the current player.
+	 * @param categoryName name of the category. Example: Three Of A Kind
+	 * @return a specific ScoreCategory for the current player based on a String
+	 */
+
+	public ScoreCategory getCategory(String categoryName) {
+		return getCurrentPlayer().getScoreboard().getCategory(categoryName);
+	}
+	/**
+	 * Get the score for a specific category based on the name of the category.
+	 * @param categoryName Name of the category
+	 * @return integer of the current score for the chosen category.
+	 */
+
+	public int getScoreForCategory (String categoryName) {
+		ScoreCategory category = getCategory(categoryName);
+		return category.getScore();
+	}
+
+	/**
+	 * Set the score of a specific category based on the dice aside.
+	 * @param diceAside List of the dice which have been placed aside by the user.
+	 * @param categoryName of the category needed to define the score of the dice. example: Three Of a Kind
+	 */
+	public void setScore(List<Integer> diceAside, String categoryName) {
+		ScoreCategory category = this.getCategory(categoryName);
+		int[] array = new int[5];
+		for (int i = 0; i < diceAside.size(); i++) {
+			array[i] = diceAside.get(i);
+		}
+		category.inputdice(array);
+	}
+
+	/**
+	 * Changes the current active player to the next player in the PlayerGroup List
+	 * Sets the isActive boolean to true for the current player
+	 * Increases the currentPlayerIndex of the playergroup to loop through all the different players. If the end of the list is reached
+	 * resets the index to 0.
+	 * Creates a new Turn object for the new active player
+	 */
+
+	public void switchToNextPlayer() {
+		getPlayerGroup().switchToNextPlayer();
+	}
 }
