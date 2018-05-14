2353a942b8ec2bd587d8720801bf6a466331e281
Raf Cloesen <raf.cloesen@student.ucll.be>
2018-05-14 21:41:45 +0200

    Hot-fix: Could register after mayRegister == false
    UI would still allow one more player to register after
    mayRegister() == false.

--- OOO_Part2/yahtzee/src/yahtzee/controller/Controller.java
+++ OOO_Part2/yahtzee/src/yahtzee/controller/Controller.java
@@ -3,6 +3,8 @@ package yahtzee.controller;
 import java.util.Map;
 import java.util.HashMap;
 import javafx.event.Event;
+import javafx.scene.control.Alert;
+import static javafx.scene.control.Alert.AlertType.ERROR;
 
 import yahtzee.domain.YahtzeeFacade;
 import yahtzee.ui.RegisterUI;
@@ -32,14 +34,24 @@ public class Controller {
     }
 
     private void registerCallback(String name) {
-	if ( ! name.equals("") && ! yahtzee.playerAlreadyRegistered(name)) {
-	    yahtzee.registerPlayer(name);
+	if ( ! yahtzee.mayRegister()) {
+	    // Request is invalid.
+	    Alert error = new Alert(ERROR);
+	    error.setTitle("Registry failed");
+	    error.setHeaderText(null);
+	    error.setContentText("Oeps, the game is no longer open for registry!");
 
-	    createBoard(name);
-	}
+	    error.show();
+	} else {
+	    if ( ! name.equals("") && ! yahtzee.playerAlreadyRegistered(name)) {
+		yahtzee.registerPlayer(name);
 
-	if (yahtzee.mayRegister()) {
-	    (new RegisterUI(this::registerCallback)).show();
+		createBoard(name);
+	    }
+
+	    if (yahtzee.mayRegister()) {
+		(new RegisterUI(this::registerCallback)).show();
+	    }
 	}
     }
 
