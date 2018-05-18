e8ccca303fc8b0a0494431c5f07c5656ffd9b7f3
KristofVandenbroucke <Kristof.vandenbrouck@gmail.com>
2018-05-18 15:14:31 +0200

    Chnage Turn to be variable of the facade De Turn klasse verplaatst zodat hij een IV is van de facade. Hierdoor wordt er een lange lijn getters vermeden om via player tot de turn te komen

--- OOO_Part2/yahtzee/src/yahtzee/domain/Turn.java
+++ OOO_Part2/yahtzee/src/yahtzee/domain/Turn.java
@@ -4,61 +4,53 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Turn {
-    private List<Integer> dicesAside;
-	private List<Integer> dicesThrown;
+    private List<Integer> diceAside;
+	private List<Integer> diceThrown;
     private int counterThrows;
 
     public Turn() {
     	counterThrows = 0;
-		dicesThrown = new ArrayList<>(5);
-		dicesAside = new ArrayList<Integer>(5);
+		diceThrown = new ArrayList<>(5);
+		diceAside = new ArrayList<>(5);
     }
 
-    public List<Integer> roll() throws DomainException {
+    public void roll() throws DomainException {
         if (mayRoll()) {
             counterThrows++;
-            dicesThrown = new ArrayList<>();
-			Throw lastThrow = new Throw(5 - getDicesAside().size());
-			dicesThrown = lastThrow.getValues();
+            diceThrown = new ArrayList<>();
+            for (int i = 0; i< (5 - getDicesAside().size()); i++) {
+            	diceThrown.add(new Die().getEyes());
+			}
             if (counterThrows == 3) {
-            	dicesAside.addAll(dicesThrown);
-            	dicesThrown.clear();
+            	diceAside.addAll(diceThrown);
+            	diceThrown.clear();
 			}
         }
         else {
             throw new DomainException("Player is not allowed to roll anymore");
         }
-        return dicesThrown;
     }
 
     public void setAside(int dieValue) throws DomainException {
-        if (dicesAside.size() > 5 ) {
+        if (diceAside.size() > 5 ) {
             throw new DomainException("Dices aside can't be larger than 5");
         }
-        int die = dicesThrown.remove(dicesThrown.indexOf(dieValue));
-        dicesAside.add(die);
+        int die = diceThrown.remove(diceThrown.indexOf(dieValue));
+        diceAside.add(die);
     }
 
     public List<Integer> getDicesAside() {
-        return this.dicesAside;
+        return this.diceAside;
     }
 
     public List<Integer> getDicesThrown() {
-        return this.dicesThrown;
+        return this.diceThrown;
     }
 
     public boolean mayRoll() {
-        if (dicesAside.size() < 5 && counterThrows < 3) {
+        if (diceAside.size() < 5 && counterThrows < 3) {
             return true;
         }
         return false;
-    }
-
-    public int getCounterThrows() {
-        return counterThrows;
-    }
-
-    public void setCounterThrows(int counterThrows) {
-        this.counterThrows = counterThrows;
     }
 }
